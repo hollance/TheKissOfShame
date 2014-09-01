9f3ef1585410ca27d8312b92120bee83c9d1393d
Brian Hansen <brian.hansen78@gmail.com>
2014-09-01 16:38:19 -0700

    initial commit for kiss of shame

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,227 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+KissOfShameAudioProcessor::KissOfShameAudioProcessor()
+{
+    inputSaturation = 1.0;
+    shame = 1.0;
+    hiss = 1.0;
+    
+}
+
+KissOfShameAudioProcessor::~KissOfShameAudioProcessor()
+{
+}
+
+//==============================================================================
+const String KissOfShameAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+int KissOfShameAudioProcessor::getNumParameters()
+{
+    return 0;
+}
+
+float KissOfShameAudioProcessor::getParameter (int index)
+{
+    // This method will be called by the host, probably on the audio thread, so
+    // it's absolutely time-critical. Don't use critical sections or anything
+    // UI-related, or anything at all that may block in any way!
+    switch (index)
+    {
+        case inputSaturationParam:     return inputSaturation;
+        case shameParam:               return shame;
+        case hissParam:                return hiss;
+            
+        default:                       return 0.0f;
+    }
+
+    return 0.0f;
+}
+
+void KissOfShameAudioProcessor::setParameter (int index, float newValue)
+{
+    // This method will be called by the host, probably on the audio thread, so
+    // it's absolutely time-critical. Don't use critical sections or anything
+    // UI-related, or anything at all that may block in any way!
+    switch (index)
+    {
+        case inputSaturationParam:     inputSaturation = newValue;  break;
+        case shameParam:               shame = newValue;  break;
+        case hissParam:                hiss = newValue;  break;
+            
+        default:            break;
+    }
+}
+
+const String KissOfShameAudioProcessor::getParameterName (int index)
+{
+    return String::empty;
+}
+
+const String KissOfShameAudioProcessor::getParameterText (int index)
+{
+    return String::empty;
+}
+
+const String KissOfShameAudioProcessor::getInputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+const String KissOfShameAudioProcessor::getOutputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+bool KissOfShameAudioProcessor::isInputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool KissOfShameAudioProcessor::isOutputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool KissOfShameAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool KissOfShameAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool KissOfShameAudioProcessor::silenceInProducesSilenceOut() const
+{
+    return false;
+}
+
+double KissOfShameAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int KissOfShameAudioProcessor::getNumPrograms()
+{
+    return 0;
+}
+
+int KissOfShameAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void KissOfShameAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String KissOfShameAudioProcessor::getProgramName (int index)
+{
+    return String::empty;
+}
+
+void KissOfShameAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void KissOfShameAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void KissOfShameAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+void KissOfShameAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    for (int channel = 0; channel < getNumInputChannels(); ++channel)
+    {
+        float* channelData = buffer.getWritePointer (channel);
+        
+        //simple clipping to test DSP
+        for(int i = 0; i < buffer.getNumSamples(); i++)
+        {
+            if (channelData[i] > 0.01f)
+                channelData[i] = 0.01f;
+            else if (channelData[i] < - 0.01f)
+                channelData[i] = -0.01f;
+            
+            //Testing GUI parameters: input, shame, and hiss.
+            channelData[i] *= inputSaturation*20;
+            channelData[i] *= shame*20;
+            channelData[i] *= hiss*20;
+        }
+    }
+
+    // In case we have more outputs than inputs, we'll clear any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    for (int i = getNumInputChannels(); i < getNumOutputChannels(); ++i)
+    {
+        buffer.clear (i, 0, buffer.getNumSamples());
+    }
+}
+
+//==============================================================================
+bool KissOfShameAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* KissOfShameAudioProcessor::createEditor()
+{
+    return new KissOfShameAudioProcessorEditor (this);
+}
+
+//==============================================================================
+void KissOfShameAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void KissOfShameAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new KissOfShameAudioProcessor();
+}
